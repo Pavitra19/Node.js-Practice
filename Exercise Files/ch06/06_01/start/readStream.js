a03bddc07822420479da76e1dc8f59cae55accf9
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-04-01 23:15:09 -0400

    Ch 5 & 6

--- Exercise Files/ch06/06_01/start/readStream.js
+++ Exercise Files/ch06/06_01/start/readStream.js
@@ -1,4 +1,25 @@
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./assets/lorum-ipsum.md", "UTF-8");
+
+let fileTxt = "";
+
 console.log("type something...");
-process.stdin.on("data", data => {
+readStream.on("data", data => {
   console.log(`I read ${data.length - 1} characters of text`);
+
+  // add up all the chunks of data
+  fileTxt += data;
+});
+
+// First little bit of data from the file
+readStream.once("data", data => {
+  console.log(data);
+});
+
+// streams also raise an end event 
+readStream.on("end", () => {
+  console.log("Finished reading file");
+  console.log(`In total I read ${fileTxt.length - 1} characters of text`);
+
 });
